0b8cd1b9c55a579cf117fdf7558b4b49b33b6835
Moonkaman <nickbatmanglidj@gmail.com>
2019-01-23 15:21:19 -0700

    Add back to friends button on friend page

--- friends/src/components/Friend.jsx
+++ friends/src/components/Friend.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "../../node_modules/@fortawesome/fontawesome-free/css/all.css";
 
 const Friend = props => {
   const friendData = props.friends.find(
@@ -8,10 +11,18 @@ const Friend = props => {
   if (friendData === undefined) return <h1>Friend Not Found</h1>;
 
   return (
-    <div>
-      <h1>{friendData.name}</h1>
-      <h2>Age: {friendData.age}</h2>
-      <h3>Email: {friendData.email}</h3>
+    <div className="friend-cont">
+      <div className="friend-header">
+        <Link to="/friends" className="back-to-friends">
+          {"< Back to Friends"}
+        </Link>
+        <i className="fas fa-user-circle" />
+        <h1>{friendData.name}</h1>
+      </div>
+      <div className="friend-content">
+        <h2>Age: {friendData.age}</h2>
+        <h3>Email: {friendData.email}</h3>
+      </div>
     </div>
   );
 };
